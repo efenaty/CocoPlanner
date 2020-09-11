096ba41cd5083342b73f49d18239b0b3db1fc554
Shahrzad <shashe@student.chalmers.se>
2020-09-11 11:45:59 +0200

    Adding new functions

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ var Schema = mongoose.Schema;
 
 //Define userSchema
 var userSchema = new Schema({
-    username : { type : String , required : true , minlength : 8 },
+    username : { type : String , required : true , unique : true },
     password : { type : String , required : true , minlength : 8 },
     email : { type : String , required : true },
     birthDate : { type : Date }
