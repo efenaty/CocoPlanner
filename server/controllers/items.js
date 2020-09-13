a0ef38f948a3de5779aea110659af0875310c616
samaraf <samar.saez@gmail.com>
2020-09-13 20:01:57 +0200

    added get items method

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -6,12 +6,22 @@ var Item = require('../models/item');
 
 //Create an item  
 router.post('/lists/items',function(req,res,next){
-    var itam = new Itam(req.body);
+    var item = new Item(req.body);
     item.save(function(err){
         if(err){
              return next(err);
             }
         res.status(201).json(item);
+    });
+});
+
+//View all items
+router.get('/lists/items', function (req,res,next){
+    Item.find(function (err, items) {
+        if(err){
+            return next(err);
+        }
+        res.json({"items" : items})
     });
 });
 
