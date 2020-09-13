b2da27e55d7017bf12264bde577002014b4c8683
Shahrzad <shashe@student.chalmers.se>
2020-09-13 13:07:56 +0200

    List's functions added

--- server/controllers/lists.js
+++ server/controllers/lists.js
@@ -14,19 +14,59 @@ router.post('/lists',function(req,res,next){
     });
 });
 
-//Delete a list 
-router.delete('lists/:id',function(req,res,next) {
+//Show all the normal lists 
+router.get('/lists', function(req, res, next) {
+    List.find({ is_favorite_list : "false" },function(err, lists) {
+        if(err) {
+            return next(err);
+        }
+        if(list==null){
+            return res.status(404).json({"message":"Unfortunately the list was not found"});
+        }
+        res.json({"The normal lists are ": lists});
+    });
+});
+
+//Show all the favorite lists
+router.get('/lists/fav', function(req, res, next) {
+    List.find({ is_favorite_list : "true" },function(err, lists) {
+        if(err) {
+            return next(err);
+        }
+        if(list == null){
+         return res.status(404).json({"message":"Unfortunately the list was not found"});
+        }
+        res.json({"The favorite lists are ": lists});
+    });
+});
+
+//Show a certain list
+router.get('/lists/:id' , function(req,res,next){
+    var id = req.params.id;
+    List.findById({ _id : id }) , function(err,lists){
+        if (err) {
+            return next(err);
+        }
+        if (list === null) {
+            return res.status(404).json({"message":"Unfortunately the list was not found"});
+        }
+        res.json({ lists});
+    }
+});
+
+//Delete a certain list 
+router.delete('/lists/:id',function(req,res,next) {
     var id = req.params.id;
     User.findByIdAndDelete({_id : id }),function(err,user) {
         if (err) {
             return next(err);
         }
-        if (user === null) {
+        if (list === null) {
             return res.status(404).json({"message":"Unfortunately the list was not found"});
         }
         res.json.list;    
     }
-})
+});
 
 
 module.exports = router;
