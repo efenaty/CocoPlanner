391061eddb902760cc963639188649ddd63811cf
samaraf <samar.saez@gmail.com>
2020-09-28 19:35:33 +0200

    pushing the views again

---
+++ client/src/views/Account.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1> we will have our account here </h1>
+</template>
+
+<script>
+export default {
+  name: 'account'
+}
+</script>
+
+<style scoped>
+ h1 {
+     color: purple;
+ }
+ </style>
---
+++ client/src/views/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1> we will have our Tasks here </h1>
+</template>
+
+<script>
+export default {
+  name: 'tasks'
+}
+</script>
+
+<style scoped>
+ h1 {
+     color: aquamarine;
+ }
+ </style>
