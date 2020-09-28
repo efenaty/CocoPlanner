a99d19eae9a3bdd676f77eac3d2516aa254d2d4d
samaraf <samar.saez@gmail.com>
2020-09-28 19:20:16 +0200

    Added a navbar and calendar/tasks/favorites/account views

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
+    <!-- <div id="nav">
+      <router-link to="/">Home </router-link>
+      <router-link to="/calendar">Calendar</router-link>
+    </div> -->
+    <div>
+    <b-nav tabs justified>
+    <b-nav-item to ="/">Home</b-nav-item>
+    <b-nav-item to = "/calendar">Calendar</b-nav-item>
+    <b-nav-item to = "/tasks">Tasks</b-nav-item>
+    <b-nav-item to = "/favorites">Favorites</b-nav-item>
+    <b-nav-item to = "/account">Account</b-nav-item>
+    <!-- <b-nav-item disabled>Disabled</b-nav-item> -->
+    </b-nav>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Calendar from './views/Calendar.vue'
+import Tasks from './views/Tasks.vue'
+import Favorites from './views/Favorites.vue'
+import Account from './views/Account.vue'
 
 Vue.use(Router)
 
@@ -12,6 +16,26 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: Calendar
+    },
+    {
+      path: '/tasks',
+      name: 'tasks',
+      component: Tasks
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: Favorites
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: Account
     }
   ]
 })
