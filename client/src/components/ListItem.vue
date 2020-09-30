c181c28e6916c74c052c262cb1c4286897820780
enti <enti@student.chalmers.se>
2020-09-30 11:07:27 +0200

    Removed unnecessary codes

--- client/src/components/ListItem.vue
+++ client/src/components/ListItem.vue
@@ -7,16 +7,7 @@
       <h4 class="mb-0">{{list.name}}</h4>
     </template>
 
-    <b-card-body>
-      <b-card-title>sth</b-card-title>
-      <b-card-sub-title class="mb-2">Card Sub Title</b-card-sub-title>
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </b-card-text>
-    </b-card-body>
-
-    <b-list-group flush>
+<b-list-group flush>
       <b-list-group-item>First</b-list-group-item>
       <b-list-group-item>Second thing</b-list-group-item>
       <b-list-group-item>Third thing</b-list-group-item>
