61f718373bd87008fa7b5b9befe61dfb71a7c10f
Shahrzad <shashe@student.chalmers.se>
2020-10-02 10:57:13 +0200

    Add Login.vue

---
+++ client/src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <b-form @submit="onSubmit" v-if="show">
+      <b-form-group id="input-1" label="Username" label-for="input-1">
+        <b-form-input id="input-1" v-model="form.username" placeholder="Enter your new username"></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-2" label="Password:" label-for="input-2">
+        <b-form-input id="input-2" v-model="form.password" type="password" placeholder="Enter your new password"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">log in</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+
+import { Api } from '@/Api'
+
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      Api.post('/login', this.form)
+        .then((result) => {
+          console.log(result)
+          // Saving the user's _id
+          const objectId = result.data._id
+          // Clear the local storage before saving the _id
+          localStorage.clear()
+          // Saving the objectId in the local storage
+          localStorage.setItem('objectId', objectId)
+          this.$router.push('/')
+        }).catch(error => {
+          console.error(error)
+        // TODO: display error message
+        })
+        .then(() => {
+        //   This code is always executed at the end. After success or failure.
+        })
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+    width: 30%;
+    height: 30%;
+    margin: auto;
+    display: inline-block;
+}
+</style>
