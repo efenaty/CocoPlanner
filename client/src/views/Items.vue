9f5a4d403c730e02447024f2c2adc53a7d3085d3
enti <enti@student.chalmers.se>
2020-10-20 18:34:23 +0200

    Fixes

--- client/src/views/Items.vue
+++ client/src/views/Items.vue
@@ -55,8 +55,8 @@ export default {
         { value: null, text: 'Please select a rating number' },
         { value: 1, text: '1' },
         { value: 2, text: '2' },
-        { value: 2, text: '3' },
-        { value: 2, text: '4' },
+        { value: 3, text: '3' },
+        { value: 4, text: '4' },
         { value: 5, text: '5' }
       ]
     }
