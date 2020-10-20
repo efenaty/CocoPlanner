3e162a6e4dfb64697bc0741dd029774680cbbf0f
enti <enti@student.chalmers.se>
2020-10-20 19:42:45 +0200

    Homepage changes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <b-container>
     <h1>{{message}}</h1>
+    <h2> Welcome to My Coco Planner!</h2>
+    <p> Organize your daily to-dos, and list your favorite things! </p>
+    <img src="../assets/logo.png">
   </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import { Api } from '@/Api'
+// import LottieAnimation from 'lottie-vuejs' // import lottie-vuejs
+
 export default {
   name: 'home',
   data() {
@@ -14,6 +19,9 @@ export default {
       message: ''
     }
   },
+  // components: {
+  //   LottieAnimation
+  // },
   mounted() {
     const id = localStorage.getItem('objectId')
     Api.get(`/users/${id}`)
